e9fb98ca9c70f656cd5964f0a33ae671bc3f27f2
Willian <willan.dafonseca@hotmail.com>
2021-08-17 13:06:51 -0300

    :sparkles: Add request status

--- index.js
+++ index.js
@@ -12,34 +12,54 @@ let clients = [
     { id: 4, nome: 'Willian Fonseca', telefone: '53981490055' },
 ]
 
-app.get('/clients', (req, res) => res.json(clients))
+function log (request, response, next){
+    const {url, method} = request;
+    console.log(`${method} - ${url} at ${new Date()}`)
+    return next();
+}
+app.use(log)
+
+app.get('/clients', (request, response) => response.status(200).json(clients))
 
 
-app.get('/clients/:id', (req, res) =>
-    res.json(clients.filter(value => value.id == req.params.id)))
+app.get('/clients/:id', (request, response) => {
+    const {id} = request.params;
+    const client = clients.find(value => value.id == id);
+    if(client == undefined){
+        response.status(400).json({error: 'Requisição inválida'});
+    }else{
+        response.status(200).json(client);
+    }
+})
 
-app.post('/clients', (req, res) => {
-    const client = req.body;
+app.post('/clients', (request, response) =>{
+    const client = request.body;
     clients.push(client);
-    res.json(client);
+    response.status(201).json(client);
 })
 
+app.put('/clients/:id', (request, response) => {
+    const id = request.params.id;
+    const nome = request.body.nome;
 
-app.put('/clients/:id', (req, res) => {
-    const id = req.params.id;
-    const nome = req.body.nome;
-
-    let client = clients.filter(value => value.id == id);
-
-    client[0].nome = nome;
-
-    res.json(client[0]);
+    let client = clients.find(value => value.id == id);
+    if(client == undefined){
+        response.status(400).send();
+    }else{
+        client.nome = nome;
+        response.status(200).json(client);
+    }
 })
 
-app.delete('/clients/:id', (req, res) => {
-    const id = req.params.id;
-    clients = clients.filter(value => value.id != id);
-    res.json(clients);
-})
+app.delete('/clients/:id', (request, response) => {
+    const {id} = request.params;
+    const index = clients.findIndex(value => value.id == id);
+    if(index == -1){
+        response.status(400).send();
+    }else{
+        clients.splice(index,1);
+        response.status(204).send();
+    }
 
+})
 app.listen(3000);
